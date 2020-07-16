57049fa810a54052554c55de137f6eb3c2495548
OlaDziedziuch <44896083+OlaDziedziuch@users.noreply.github.com>
2020-07-16 10:42:23 +0200

    Create zaprojektuj-koszulke.pl

---
+++ 5-exploratory-testing/zaprojektuj-koszulke.pl
@@ -0,0 +1,97 @@
+Testowanie eksploracyjne
+ 
+ADRES STRONY:
+zaprojektuj-koszulke.pl
+
+ŚRODOWISKO TESTOWE:
+Windows 10 Home 1930 64-bit, Chrome 80.0.3987.149
+
+DATA, CZAS:
+15.04.2020 od godz. 00:00 do 1:00.
+
+OBSZAR TESTOWANIA:
+testowanie funkcjonalności,
+testowanie użyteczności (UX/UI).
+ 
+OCENA JAKOŚCI:
+Pomimo krótkiego czasu testowania stwierdzono wiele defektów, które w znacznym stopniu mogą oddziaływać na odbiór marki. W raporcie uwzględniono 14 defektów, z czego 4 został nadany priorytet wysoki. Po przeprowadzeniu testów zaleca się przebudowanie strony pod kątem strukturalnym, jak i samej zawartości. Szczegółowe sugestie znajdują się na końcu raportu.
+ 
+ZNALEZIONE DEFEKTY: 
+ 
+Strona główna oraz strona produktu
+ 
+STRONA GŁÓWNA: Napis ‘Zaprojektuj Nadruk’ powinien brzmieć ‘Zaprojektuj nadruk’. W  języku polskim rzeczowniki pisze się małą literą.
+ 
+PRIORYTET: niski
+
+STRONA PRODUKTU: Klikając w powyższy przycisk, który znajduje się pod zdjęciem ‘Dla Niego’ otwiera się strona z kreatorem nadruku. W  treści jest zawarta niepotrzebna informacja o koszcie koszulki wynoszącym  19,00 - 29.00 zł. W rzeczywistości za przy wyborze każdej opcji koszt koszulki nie wynosi więcej niż 19 zł.
+ 
+PRIORYTET: niski
+
+STRONA PRODUKTU: W tym samym oknie produktu istnieje funkcja ‘Wysyłka’. Po najechaniu kursorem na szczegóły opisu pojawia się napis ‘Option description’. Warto zmienić ten opis, doprecyzowując informacje dotyczące procesu wysyłki.
+
+PRIORYTET: średni 
+
+STRONA GŁÓWNA: Klikając w przycisk ‘Dla Niej’ pojawia się strona z kreatorem o nazwie  ‘Wybierz opcje’ - zamówienie na koszulkę damską. W tym przypadku występuje błąd językowy - przymiotnik damska powinien być pisany małą literą.
+ 
+PRIORYTET: niski
+ 
+STRONA PRODUKTU - okno ‘wybierz opcje’. Defektem jest możliwość zatwierdzenia opcji zaprojektowania (a tym samym zakupu) koszulki, przy braku określenia koloru koszulki. W tym przypadku przycisk ‘Zatwierdź opcje’ nie powinien być aktywny. Po kliknięciu tego przycisku pojawia się koszulka w kolorze Royal Blue, zaś na wizualizacji znajduje się koszulka w kolorze białym.
+ 
+PRIORYTET: wysoki
+ 
+STRONA GŁÓWNA: Tekst znajdujący się na przycisku ‘Uruchom kreatora koszulek’ jest niepoprawny. Przycisk powinien mieć nazwę: ‘Uruchom kreator koszulek’ (jest to mianownik, nie dopełniacz).
+ 
+PRIORYTET: niski
+ 
+STRONA GŁÓWNA: Przy najechaniu na produkt ‘Koszulka Męska Organiczna’ pojawia się tzw. efekt hover. Efekt przejścia, który znajduje się na zdjęciu koszulki sprawia, że jeżeli użytkownik najedzie na niego myszką, pojawia się ucięte zdjęcie.
+ 
+PRIORYTET: średni
+
+STRONA GŁÓWNA: Brak ikon Social Media (m.in. Facebook, Twitter, Google+), które są widoczne w wersji mobilnej na Smartfonie.
+ 
+PRIORYTET: średni
+
+9. STRON GŁÓWNA: Niedziałające linki do ‘ Zwrotów i reklamacji’ oraz ‘Regulaminu’, znajdujące się w stopce strony.
+ 
+PRIORYTET: wysoki
+ 
+Kreator
+ 
+10. Okno wskazujące ‘ilość’ nie są limitowane. Użytkownik maksymalnie może wprowadzić 1e+21 zamówień, podczas gdy przycisk ‘Do koszyka’ pozostaje aktywny.
+ 
+PRIORYTET: wysoki 
+
+Strona zamówienia
+
+11. Po kliknięciu w link ‘Kliknij tu, aby się zalogować’ napis  zmienia kolor na biały, tym samym pozostaje niewidoczny dla użytkownika, gdyż kolor tła jest taki sam.
+
+PRIORYTET: średni
+ 
+12. Formularz ‘Dane płatności’ jest pisany naprzemiennie w języku polskim oraz w języku angielskim. Należy to ujednolicić.
+
+PRIORYTET: średni
+
+13. Brak walidacji danych dla pól -  ‘postcode’ oraz ‘telefon’. Wartości te powinny przybierać jedynie typ danych liczbowych. Próba wpisu ciągu znaków powinna zakończyć się podobnie jak w przypadku pola ‘Email Address’.
+ 
+PRIORYTET: średni
+
+Zakładka PREZENT
+ 
+14. Przycisk ‘Personalizuj’ w zakładce PREZENT jest nieaktywny. 
+ 
+PRIORYTET: wysoki
+
+SUGESTIE
+ 
+Powinna być zakładka - ‘Jak wykonać projekt?’ w której, znajdowałyby się wszystkie  niezbędne informacje z zakresu danych technicznych  potrzebne do realizacji zamówienia. Chociażby poniższa informacja powinna znajdować się w takim miejscu, nie zaś na początku strony głównej:
+
+ 
+Brak spójności w przyciskach znajdujących się na głównej stronie. Niektóre z nich posiadają efekt najechania,  inne nie. To samo tyczy się ich kolorystyki.
+Niespójna kolorystyka całego serwisu. Warto wybrać max 3 do 5 kolorów przewodnich.
+Strona zawiera bardzo dużą ilość treści, która w wielu miejscach się powtarza. Bardzo dużo błędów językowych i gramatycznych. Zaleca się skorzystanie z usług copywritera.
+Wszystkie informacje na stronie powinny być napisane w j. polskim.
+Brak ikon Social Media może powodować słabsze pozycjonowanie w wyszukiwarce.
+Brak przykładowych realizacji. 
+
+Całą stronę najlepiej podsumowuje screen wykonanego zamówienia, czyli “Witryna ma problemy techniczne’. 
