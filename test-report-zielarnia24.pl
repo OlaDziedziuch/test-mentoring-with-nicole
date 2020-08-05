83ded3c9b4bd9bfe9f0f7b6b0ef10c8000870bf5
OlaDziedziuch <44896083+OlaDziedziuch@users.noreply.github.com>
2020-08-05 12:45:12 +0200

    Create test-report-zielarnia24.pl

---
+++ test-report-zielarnia24.pl
@@ -0,0 +1,35 @@
+Test report
+
+Summary of actions performed:
+Website: https://zielarnia24.pl/index.html
+
+Test environment: 
+Windows 10 Home 1930 64-bit, Chrome 80.0.3987.149
+
+Date and time: 
+14.05.2020 11-12 am (60 min)
+
+Areas of application and types of tests performed: 
+functional and usability
+
+Testing area: 
+Home page and Product page
+
+Types of test done: 
+functional
+
+Time: 
+~  45 min (home page) ~ 15 min (product page)
+
+Summary of all defects
+
+Defects found during exploratory testing: 
+
+ID_1 - “Do koszyka” button is not visible for out of stock products” (page 2).
+ID_2 - “Quantity functionality is not implemented in the product card” (page 3).
+
+Summary of test completion criteria:
+The assumed realizations have been made - during 60 minutes of testing completely checked all functionalities on home page and partly product card.
+At this time were found two defects (details are included in exploratory testing report). Additionally was found another one, which is related to the lack of appearance of the Facebook window. 
+During the performance of the approval scenario in the incognito mode this problem did not appear. 
+The estimated time of 60 minutes allowed to completely test the functionality and usability of the home page, however the rest of the website requires thorough time (approx. 30-40 minutes more). During this time the purchasing process should be tested.
